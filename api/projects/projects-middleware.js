c41766a63a2b0adf3f4437905f695246ccb39c60
Jesse-Leegwater <Jleegwater@live.com>
2021-09-03 14:54:40 -0700

    fixed some

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,27 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model");
+
+function logger(req, res, next) {
+  console.log(req.method);
+  console.log(req.url);
+  console.log(Date.now());
+  next();
+}
+
+function validateId(req, res, next) {
+  const { id } = req.params;
+  Projects.get(id).then((possibleProject) => {
+    if (possibleProject) {
+      req.project = possibleProject;
+      next();
+    } else next({ message: "Project not found", status: 404 });
+  });
+}
+function validatePost(req, res, next) {
+  req.body.name && req.body.description
+    ? next()
+    : next({ message: "missing name or description!", status: 400 });
+}
+
+// do not forget to expose these functions to other modules
+module.exports = { logger, validateId, validatePost };
--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+const cors = require("cors");
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+//const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 
+server.use(express.json());
+server.use(cors());
+
+//server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 module.exports = server;
